6dccddf51cbc242986f2bf59fbd1485eeab04021
NazarShaa <160277049+NazarShaa@users.noreply.github.com>
2024-03-22 23:56:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,40 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  int left = 0, right = size, temp = size/2, k = 0;
-  while (temp != 0) {
-    if (arr[temp] > value)
-      right = temp;
-    else 
-      left = temp;
-    temp /= 2;
-  }
-  for (int i = left; i < right; i++) {
-    if (arr[i] == value)
-      k++;
-  }
-  return k;
+int cbinsearch(int* arr, int size, int value) {
+	int left = 0, right = size, temp = size / 2, k = 0;
+	while (true) {
+		if (arr[temp] > value) {
+			right = temp;
+			temp -= (right - left) / 2;
+		}
+		else if (arr[temp] == value) {
+			for (int i = temp; i > 0; i--) {
+				if (arr[i] != value) {
+					left = i + 1;
+					break;
+				}
+			}
+			for (int i = temp; i < size; i++) {
+				if (arr[i] != value) {
+					right = i - 1;
+					break;
+				}
+			}
+			left = 0;
+			right = size;
+			break;
+		}
+		else {
+			left = temp;
+			temp += (right - left) / 2;
+		}
+
+
+	}
+	for (int i = left; i < right; i++) {
+		if (arr[i] == value)
+			k++;
+	}
+	return k;
 }
 
